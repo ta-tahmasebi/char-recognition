b33173e8c5825723d2c0e84556d52252c4785d85
Amirmahdi <gm.ta.tahmasebi@gmail.com>
2025-09-09 19:24:53 +0330

    temp

---
+++ src/UI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by amirmahdi on 9/9/25.
+//
+
+#include "UI.h"
---
+++ src/UI.h
@@ -0,0 +1,14 @@
+//
+// Created by amirmahdi on 9/9/25.
+//
+
+#ifndef APP_UI_H
+#define APP_UI_H
+
+
+class UI {
+
+};
+
+
+#endif //APP_UI_H
